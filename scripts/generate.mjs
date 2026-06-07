a977a13106fb42e97df3e2c451a8141f18b37884
Kento Nishi <kento24gs@outlook.com>
2026-06-06 21:56:02 -0400

    carry cache bust through token links

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,16 +1,17 @@
 import { writeFile } from "node:fs/promises";
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+const build = Date.now().toString(36);
 
 let body = `<nav>
 <a href="/">RESET</a>
-<a href="./~/">DONE</a>
+<a href="./~/?v=${build}">DONE</a>
 </nav>
 <ul>
 `;
 
 for (const ch of alphabet) {
-  body += `<li><a href="./${ch}/">${ch}</a></li>\n`;
+  body += `<li><a href="./${ch}/?v=${build}">${ch}</a></li>\n`;
 }
 
 body += `</ul>
